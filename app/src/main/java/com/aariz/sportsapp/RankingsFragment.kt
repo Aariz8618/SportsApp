721a615c294f721ee7cfe3f57acf37b3b1b6d0f3
Aariz Kazi <aarizkazi7002@gmail.com>
2025-07-24 11:55:18 +0530

    Aariz Commit

---
+++ app/src/main/java/com/aariz/sportsapp/RankingsFragment.kt
@@ -0,0 +1,33 @@
+package com.aariz.sportsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aariz.sportsapp.databinding.FragmentRankingsBinding
+
+class RankingsFragment : Fragment() {
+
+    private var _binding: FragmentRankingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRankingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
